77a897f710867e7bf79b5697f17f09440f3d21a2
ckoch786 <yunusabd786@gmail.com>
2013-03-03 15:42:05 -0500

    --

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,8 +9,7 @@
 (server-start)
 
 ; Load solarized color themes
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized"
-)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
@@ -44,6 +43,8 @@
 (require 'list-register)
 (global-set-key (kbd "C-x r v") 'list-register)
 
+(defalias 'list-buffers 'ibuffer) ; always use ibuffer
+
 
 ; Change major mode 
 (setq auto-mode-alist
@@ -67,7 +68,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+ '(org-mobile-files (quote (org-agenda-files "" "~/.emacs/org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
